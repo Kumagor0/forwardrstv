32f8a95b80f6c38052e9606aca7821a2ff3fc6ea
Gleb Gladkiy <gleba@yandex.ru>
2018-07-30 17:30:59 +0300

    feat(server): added forwarding from chat to channel

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { TG_BOT_TOKEN, INPUT_CHAT_ID, OUTPUT_CHANNEL_ID } = process.env;
+const bot = new TelegramBot(TG_BOT_TOKEN, { polling: true });
+
+bot.on('message', msg => {
+  try {
+    if (msg.chat.id == INPUT_CHAT_ID) {
+      bot.forwardMessage(OUTPUT_CHANNEL_ID, INPUT_CHAT_ID, msg.message_id);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
